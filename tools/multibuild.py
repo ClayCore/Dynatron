b9f7fd0f3f854f33b22ddad928be2877597a90a0
Mercury <deltaware@cock.li>
2020-05-12 07:33:39 +0200

    fix: paths and variable naming

--- tools/multibuild.py
+++ tools/multibuild.py
@@ -17,17 +17,17 @@ various webdev related projects.
 
 Usage:
     multibuild.py [--help]
-    multibuild.py clean (client|server)
-    multibuild.py wipe  (client|server)
-    multibuild.py postbuild (client|server)
+    multibuild.py clean (Client|Server)
+    multibuild.py wipe  (Client|Server)
+    multibuild.py postbuild (Client|Server)
 
 Options:
     --help      Shows this help screen
 
 Commands:
-    clean (client|server)       Cleans the build folder for either the client or the server project
-    wipe (client|server)        Deleted node_modules, .cache, build folders and lock files
-    postbuild (client|server)   Restructures the build/release folder.
+    clean (Client|Server)       Cleans the build folder for either the `Client` or the `Server` project
+    wipe (Client|Server)        Deleted node_modules, .cache, build folders and lock files
+    postbuild (Client|Server)   Restructures the build/release folder.
 """
 
 import errno
@@ -178,8 +178,8 @@ class Multitool(object):
     def map_files(self):
         Logger.pinfo('Collecting files by their extensions', start='\n')
 
-        if (not self.build_dir.exists()) or (not self.release_build_dir.exists())
-            Logger.perror('Production build not found!') ~
+        if (not self.build_dir.exists()) or (not self.release_build_dir.exists()):
+            Logger.perror('Production build not found!')
             Logger.exit(errno.ENOENT)
 
         # create necessary directories
@@ -224,40 +224,40 @@ class Multitool(object):
         for index_path in self.html_files:
             with open(index_path) as file:
                 for line in file:
-                        # replace all backslashes with forward slashes for convenience
-                        if '\\' in line:
-                            line = line.replace('\\', ' / ')
+                    # replace all backslashes with forward slashes for convenience
+                    if '\\' in line:
+                        line = line.replace('\\', '/')
                     temp_file.append(line)
 
         # iterate over the file, look for filenames
         # and replaces them with new paths
         for index in range(0, len(temp_file)):
-            for resource in self.resource_files:
+            for resource in self.resources:
                 file = resource.name
                 line = temp_file[index]
-                if (filename in line) and (not 'static' in line):
+                if (file in line) and (not 'static' in line):
                     # replace .css and .css.map paths
                     if '.css' in line or '.css.map' in line:
-                        temp_file[index] = line.replace(filename, str(Path('.') / self.static_dir.name / filename))
+                        temp_file[index] = line.replace(file, str(Path('.') / self.static_dir.name / file))
                     # replace .js and .js.map paths
                     elif ('.js' in line or '.js.map' in line) and (not 'manifest' in line):
-                        temp_file[index] = line.replace(filename, str(Path('.') / self.static_dir.name / filename))
+                        temp_file[index] = line.replace(file, str(Path('.') / self.static_dir.name / file))
                     # don't replace manifest file paths
                     elif ('__' in line) and ('manifest' in line):
                         temp_file[index] = line
                     # if none of the paths match, it must be an asset
                     else:
-                        for asset in self.asset_files:
+                        for asset in self.assets:
                             if (asset.suffix in line) and (not '__' in line) and (not self.asset_dir.name in line):
-                                temp_file[index] = line.replace(filename, str(Path('.') / self.asset_dir / filename))
-        
+                                temp_file[index] = line.replace(file, str(Path('.') / self.asset_dir.name / file))
+
         # open the same file and save the changes
         for index_path in self.html_files:
-            with open(index, 'w') as file:
+            with open(index_path, 'w') as file:
                 for line in temp_file:
                     # replace all backslashes with forward slashes for convenience
                     if '\\' in line:
-                        line = line.replace('\\', ' / ')
+                        line = line.replace('\\', '/')
                     file.write(line)
 
     def move_files(self, file_list, dest):
@@ -277,7 +277,7 @@ class Multitool(object):
 
     def postbuild(self):
         Logger.pinfo('Preparing for post build restructure', start='\n')
-        
+
         # Map necessary files
         self.map_files()
 
@@ -290,7 +290,7 @@ class Multitool(object):
         self.move_files(self.js_files, self.static_dir)
         self.move_files(self.js_map_files, self.static_dir)
         self.move_files(self.assets, self.asset_dir)
-        
+
         Logger.pinfo('Post build complete!', start='\n')
 
 
@@ -300,30 +300,28 @@ def main():
     for k, v in args.items():
         if v is True and k == 'clean':
             for k, v in args.items():
-                if v is True and k == 'client':
+                if v is True and k == 'Client':
                     tool = Multitool('Client')
                     tool.partial_clean()
-                elif v is True and k == 'server':
+                elif v is True and k == 'Server':
                     tool = Multitool('Server')
                     tool.partial_clean()
         elif v is True and k == 'wipe':
             for k, v in args.items():
-                if v is True and k == 'client':
+                if v is True and k == 'Client':
                     tool = Multitool('Client')
                     tool.full_clean()
-                elif v is True and k == 'server':
+                elif v is True and k == 'Server':
                     tool = Multitool('Server')
                     tool.full_clean()
         elif v is True and k == 'postbuild':
             for k, v in args.items():
-                if v is True and k == 'client':
+                if v is True and k == 'Client':
                     tool = Multitool('Client')
                     tool.postbuild()
-                elif v is True and k == 'server':
+                elif v is True and k == 'Server':
                     tool = Multitool('Server')
                     tool.postbuild()
-        else:
-            assert False, 'Unhandled option'
 
 
 if __name__ == "__main__":
